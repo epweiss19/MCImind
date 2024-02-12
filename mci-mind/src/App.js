e9aaadae1b11a81e049c2bc1924112a1bd383576
epweiss19 <100245684+epweiss19@users.noreply.github.com>
2024-02-12 16:03:03 -0500

    Vertical Arrow Movement Fixed
    Y-value calculation of arrow fixed
    Position of arrow on page changes correctly upon button press

--- mci-mind/src/App.js
+++ mci-mind/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div id="arrowObj">
-          >
+          {'>'}
         </div>
         <button onClick={handleClick}>Shoot</button>
       </header>
@@ -13,56 +13,55 @@ function App() {
   );
 }
 
-
-let t;
-
 function handleClick() {
-  alert("Clicked");
-  t = new Date().getTime();
-  console.log("Change Height")
-  heightLoop(t);
+
+  // THESE CHANGE RESOLUTION AND ANIMATION SPEED
+  const time_step   = 40;   // Decrease for smoother animation
+  const time_scale  = 300;  // Decrease for faster animation
+
+  // Get the time the click occurred
+  const start_time = Date.now();
+
+  // Continuously update the height
+  heightLoop(start_time, time_step, time_scale);
 }
 
-function heightLoop(t) {
-  let ret;
-  while (ret = changeHeight(t)) {
-    setTimeout(() => {
-      t+=100;
-      console.log(ret);
-      if (ret < -0.1) {
-        return;
-      }
-    }, 100);
+function heightLoop(start_time, time_step, time_scale) {
+
+  // Set Intial Conditions : Declare final positions
+  let yi = 0;
+  let vi = 50;
+  let new_y;
+
+  // Limited Scope for getNewHeight() : limits use of global variables
+  function getNewHeight(start_time) {
+
+    // Time Scale used to slow down the simulation
+    let localTime = (Date.now() - start_time) / time_scale;
+
+    // Update final positions
+    let new_y = yi + vi * localTime - 0.5 * 9.8 * Math.pow(localTime, 2);
+    return new_y;
   }
+
+  // SetInterval used as constant time delayed loop
+  let timerID = setInterval(() => {
+
+    // Calculate New Height
+    new_y = getNewHeight(start_time);
+
+    // Exit Handling
+    if (new_y < 0) {
+      clearInterval(timerID);
+      new_y = 0;
+    }
+    console.log(new_y);
+
+
+    // Update Position of Arrow
+    document.getElementById('arrowObj').style="position:relative; top:" + -new_y + "px;";
+
+  }, time_step);
 }
 
 export default App;
-
-let vi = 10;
-let yi = 0;
-let yf;
-let xi = 0;
-let xf;
-let localTime;
-
-function changeHeight(currentTime) {
-
-  console.log("Called")
-  localTime = currentTime - t;
-
-  yf=yi+vi*localTime-0.5*9.8*Math.pow(localTime,2);
-  // if (localTime > 0 && yf <= 0) {
-  //   yf = 0;
-  // }
- //xf = xi*vi*localTime;
-
-  console.log(`localTime = ${localTime}`);
-
-  if (yf < 0) {
-    console.log("Exiting")
-    return yf;
-  }
-  console.log(`yf = ${yf}`);
-  //changeHeight(new Date());
-  return yf;
-}
