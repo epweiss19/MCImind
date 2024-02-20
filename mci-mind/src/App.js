c8d3fa083fb495a6f59df1129fe03a0c7c9270fd
mghera02 <mghera02@hotmail.com>
2024-02-19 19:57:00 -0500

    fixed some position stuff and started on tracking overlapping

--- mci-mind/src/App.js
+++ mci-mind/src/App.js
@@ -7,6 +7,7 @@ function App() {
         <div id="arrowObj">
           {'>'}
         </div>
+        <div id="target"></div>
         <button onClick={handleClick}>Shoot</button>
       </header>
     </div>
@@ -29,8 +30,8 @@ function handleClick() {
 function heightLoop(start_time, time_step, time_scale) {
 
   // Set Intial Conditions : Declare final positions
-  let yi = 0;
-  let vi = 50;
+  let yi = -300;
+  let vi = 40;
   let new_y;
   let new_x;
 
@@ -51,8 +52,22 @@ function heightLoop(start_time, time_step, time_scale) {
     let localTime = (Date.now() - start_time) / time_scale;
 
     // Update final positions
-    let new_x = localTime * vi;
+    let new_x = 370 + localTime * vi;
     return new_x;
+  }
+
+  function checkIfOverlapping() {
+    let arrowObj = document.getElementById("arrowObj");
+    var arrowObjPos = arrowObj.getBoundingClientRect();
+    //console.log(arrowObjPos.top, arrowObjPos.right, arrowObjPos.bottom, arrowObjPos.left);
+
+    let target = document.getElementById("target");
+    var targetPos = target.getBoundingClientRect();
+    //console.log(targetPos.top, targetPos.right, targetPos.bottom, targetPos.left);
+    
+    //if(arrowObjPos.top <= targetPos.top) {
+
+    //}
   }
 
   // SetInterval used as constant time delayed loop
@@ -63,16 +78,18 @@ function heightLoop(start_time, time_step, time_scale) {
 
     new_x = getNewDistance(start_time);
 
+    checkIfOverlapping();
+
     // Exit Handling
-    if (new_y < 0) {
+    if (new_y < -300) {
       clearInterval(timerID);
-      new_y = 0;
+      new_y = -300;
     }
     console.log(new_y);
 
 
     // Update Position of Arrow
-    document.getElementById('arrowObj').style="position:relative; top:" + -new_y + "px; left: " + new_x + "px;";
+    document.getElementById('arrowObj').style="position:absolute; top:" + -new_y + "px; left: " + new_x + "px;";
 
   }, time_step);
 }
